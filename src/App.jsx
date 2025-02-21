1c8caa5c9ef896078055d05f18c65b5fb2e391b5
bakasibruceto <abla09027@gmail.com>
2025-02-22 01:30:25 +0800

    added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { handleListItemAnimations, createScrollTrigger } from "./utils.js";
-import clickSound from "../src/assets/sounds/MenuHit.wav";
-
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useLenis } from "lenis/react";
@@ -9,6 +7,11 @@ import profileImage from "../src/assets/img/profile.jpg"; // Import the image
 import profileImage320w from "../src/assets/img/profile-320w.jpg";
 import profileImage480w from "../src/assets/img/profile-480w.jpg";
 import profileImage800w from "../src/assets/img/profile-800w.jpg";
+import proj1 from "../src/assets/img/proj/hot.jpg";
+import proj2 from "../src/assets/img/proj/1.png";
+import NavigationMenu from "../src/components/NavigationMenu";
+import SocialLinks from "../src/components/SocialLinks.jsx";
+
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
@@ -46,79 +49,108 @@ const App = () => {
       { x: "0%", duration: 0.5, stagger: 0.1 } // Slide into view
     );
 
-    gsap.fromTo(
-      ".slide-down",
-      { y: "100%", scale: 2 },
-      {
-        y: "0%",
-        scale: 1,
-        ease: "none",
-        scrollTrigger: {
-          trigger: ".image-trigger",
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true,
-          onLeave: () => {
-            gsap.to(".slide-down", {
-              y: "-100%",
-              scale: 1,
-              ease: "none",
-              scrollTrigger: {
-                trigger: ".image-trigger2",
-                start: "top bottom",
-                end: "bottom top",
-                scrub: true,
-              },
-            });
-          },
-        },
-      }
-    );
+    // gsap.fromTo(
+    //   ".home-slide",
+    //   { y: "100%", scale: 2 },
+    //   {
+    //     y: "0%",
+    //     scale: 1,
+    //     ease: "none",
+    //     scrollTrigger: {
+    //       trigger: ".home-trigger",
+    //       start: "top bottom",
+    //       end: "bottom top",
+    //       scrub: true,
+    //       onLeave: () => {
+    //         gsap.to(".slide-down", {
+    //           y: "-100%",
+    //           scale: 1,
+    //           ease: "none",
+    //           scrollTrigger: {
+    //             trigger: ".image-trigger2",
+    //             start: "top bottom",
+    //             end: "bottom top",
+    //             scrub: true,
+    //           },
+    //         });
+    //       },
+    //     },
+    //   }
+    // );
 
-    gsap.fromTo(
-      ".text-slide",
-      { y: "100%" },
-      {
-        y: "0%",
-        scale: 1,
-        ease: "none",
-        scrollTrigger: {
-          trigger: ".image-trigger",
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true,
-          onLeave: () => {
-            gsap.to(".text-slide", {
-              y: "-100%",
-              scale: 1,
-              ease: "none",
-              scrollTrigger: {
-                trigger: ".image-trigger2",
-                start: "top bottom",
-                end: "bottom top",
-                scrub: true,
-              },
-            });
-          },
-        },
-      }
-    );
+    // gsap.fromTo(
+    //   ".slide-down",
+    //   { y: "100%", scale: 2 },
+    //   {
+    //     y: "0%",
+    //     scale: 1,
+    //     ease: "none",
+    //     scrollTrigger: {
+    //       trigger: ".image-trigger",
+    //       start: "top bottom",
+    //       end: "bottom top",
+    //       scrub: true,
+    //       onLeave: () => {
+    //         gsap.to(".slide-down", {
+    //           y: "-100%",
+    //           scale: 1,
+    //           ease: "none",
+    //           scrollTrigger: {
+    //             trigger: ".image-trigger2",
+    //             start: "top bottom",
+    //             end: "bottom top",
+    //             scrub: true,
+    //           },
+    //         });
+    //       },
+    //     },
+    //   }
+    // );
 
-    gsap.fromTo(
-      ".contact-slide",
-      { y: "-50%", scale: 2 },
-      {
-        y: "50%",
-        scale: 1,
-        ease: "none",
-        scrollTrigger: {
-          trigger: ".contact-trigger",
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true,
-        },
-      }
-    );
+    // gsap.fromTo(
+    //   ".text-slide",
+    //   { y: "100%" },
+    //   {
+    //     y: "0%",
+    //     scale: 1,
+    //     ease: "none",
+    //     scrollTrigger: {
+    //       trigger: ".image-trigger",
+    //       start: "top bottom",
+    //       end: "bottom top",
+    //       scrub: true,
+    //       onLeave: () => {
+    //         gsap.to(".text-slide", {
+    //           y: "-100%",
+    //           scale: 1,
+    //           ease: "none",
+    //           scrollTrigger: {
+    //             trigger: ".image-trigger2",
+    //             start: "top bottom",
+    //             end: "bottom top",
+    //             scrub: true,
+    //           },
+    //         });
+    //       },
+    //     },
+    //   }
+    // );
+
+    // gsap.fromTo(
+    //   ".contact-slide",
+    //   { y: "-50%", scale: 2 },
+    //   {
+    //     y: "50%",
+    //     scale: 1,
+    //     ease: "none",
+    //     scrollTrigger: {
+    //       trigger: ".contact-trigger",
+    //       start: "top bottom",
+    //       end: "bottom top",
+    //       scrub: true,
+    //     },
+    //   }
+    // );
 
     createScrollTrigger("aboutme", setCurrentTarget);
     createScrollTrigger("projects", setCurrentTarget);
@@ -141,74 +173,31 @@ const App = () => {
     }
   }
 
-  const playSound = () => {
-    const audio = new Audio(clickSound);
-    audio.play();
-  };
-
   return (
     <div className="main-containter text-primary bg-black/90 grainy relative overflow-hidden overflow-y-visible">
       <div className="container mx-auto">
+        <NavigationMenu handleScroll={handleScroll} />
+        <SocialLinks />
         <div id="home" className="h-screen relative overflow-hidden">
           <div className="select-none font-lexend-zetta p-5 pb-0 w-full">
             <div className="animate-header flex flex-col sm:flex-row items-center w-full text-xs 2xl:text-lg overflow-hidden">
               <div className="overflow-hidden">
                 <span className="sm:block hidden ">
-                  full-stack developer freelancer Hello world!
+                  full-stack web developer freelancer Hello world!
                 </span>
               </div>
-
               <div className="flex-grow ml-2 sm:block hidden sm:mt-0 mt-2 overflow-hidden">
                 <span className="border-b-2 border-[#F0F4F8] w-full block animate-header2"></span>
               </div>
             </div>
           </div>
 
-          <ul className="z-10 select-none cursor-pointer fixed font-lexend-zetta right-0 items-end justify-end text-2xl 2xl:text-3xl leading-relaxed pr-5">
-            <div className="overflow-hidden">
-              <li
-                className="animate-links glow-on-hover"
-                data-link="aboutme"
-                onClick={() => {
-                  playSound();
-                  handleScroll("aboutme");
-                }}
-              >
-                ABOUT ME
-              </li>
-            </div>
-            <div className="overflow-hidden glow-on-hover">
-              <li
-                className="animate-links"
-                data-link="projects"
-                onClick={() => {
-                  playSound();
-                  handleScroll("projects");
-                }}
-              >
-                PROJECTS
-              </li>
-            </div>
-            <div className="overflow-hidden glow-on-hover">
-              <li
-                className="animate-links"
-                data-link="contacts"
-                onClick={() => {
-                  playSound();
-                  handleScroll("contacts");
-                }}
-              >
-                CONTACTS
-              </li>
-            </div>
-          </ul>
-
           <div className="flex flex-col select-none font-prata sm:leading-45 2xl:leading-70 leading-25 sm:bottom-0 sm:left-0 items-center justify-center sm:absolute sm:pl-5 mt-60 overflow-hidden">
             <div
               className="header overflow-hidden reflection"
               data-text="AARON"
             >
-              <h1 className="sm:text-[10.8rem] 2xl:text-[16.2rem] text-[5.1rem] animate-text">
+              <h1 className="xl:text-[10.8rem] 2xl:text-[16.2rem] text-[5.1rem] animate-text">
                 AARON
               </h1>
             </div>
@@ -230,56 +219,12 @@ const App = () => {
             </div>
 
             <span className="sm:hidden w-[25rem] block font-lexend-zetta text-xs text-center p-4 animate-text">
-              full-stack developer freelancer
+              full-stack web developer freelancer
             </span>
-          </div>
-
-          <div className="flex gap-4 right-0 bottom-0 p-5 fixed items-center z-4">
-            <a
-              className="overflow-hidden text-white rounded-full hover:shadow-blue-400 hover:shadow-2xl"
-              href="https://github.com/bruce-agaloos"
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={() => {
-                playSound();
-              }}
-            >
-              <svg
-                className="w-10 animate-text fill-current inline-block"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-              </svg>
-            </a>
-
-            <a
-              className="overflow-hidden image-trigger rounded-lg mt-1 hover:shadow-blue-400 hover:shadow-2xl"
-              href="https://www.linkedin.com/in/agaloos-aaronbruce"
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={() => {
-                playSound();
-              }}
-            >
-              <svg
-                className="w-[2.7rem] animate-text glow-on-hover"
-                xmlns="http://www.w3.org/2000/svg"
-                width="45"
-                height="auto"
-                viewBox="0 0 25 25"
-                style={{ filter: "invert(1)" }}
-              >
-                <path
-                  fill=""
-                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
-                ></path>
-              </svg>
-            </a>
           </div>
         </div>
 
         <div id="aboutme" className="h-screen font-prata p-10 slide-text">
-          <div className="sm:block hidden 2xl:text-xl ">ABOUT ME</div>
           <div className="flex flex-col sm:flex-row items-center justify-center xl:mt-10 xl:gap-20">
             <div className="overflow-hidden z-2">
               <img
@@ -328,28 +273,105 @@ const App = () => {
                 Larvel React Tailwind MySQL
               </p>
             </div>
-            <div></div>
           </div>
         </div>
         <div
           id="projects"
-          className="h-screen font-prata p-10 flex flex-col justify-center image-trigger2"
+          className="h-auto font-prata p-10 flex flex-col image-trigger2"
         >
-          <div className="sm:block hidden">PROJECTS</div>
-          <div className="flex flex-col items-center justify-center flex-grow ">
-            <p>this part is still under development</p>
+          <div className="text-center text-5xl pb-10">PROJECTS</div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10 xl:p-15 gap-10 p-">
+            <div className="flex flex-col items-center justify-center">
+              <div className="z-3 font-lexend-zetta flex flex-col h-full">
+                <div className="border-primary border-2 p-2 h-57">
+                  <img
+                    className="opacity-60 object-cover w-full h-full"
+                    src={proj1}
+                    alt=""
+                  />
+                </div>
+                <div className="flex-grow p-2">
+                  <p className="text-lg font-semibold">eXtHealth</p>
+                  <p className="text-xs leading-loose">
+                    a browser extension tool for fact-checking health related
+                    social-media and health tips reminder
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <div className="z-3 font-lexend-zetta flex flex-col h-full">
+                <div className="border-primary border-2 p-2 h-57">
+                  <img
+                    className="opacity-60 object-cover w-full h-full"
+                    src={proj2}
+                    alt=""
+                  />
+                </div>
+                <div className="flex-grow p-2">
+                  <p className="text-lg font-semibold">CCS Forums</p>
+                  <p className="text-xs leading-loose">
+                    A community forum for College of Computing Studies students
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <div className="z-3 font-lexend-zetta flex flex-col h-full">
+                <div className="border-primary border-2 p-2 h-57">
+                  <img
+                    className="opacity-60 object-cover w-full h-full"
+                    src={proj1}
+                    alt=""
+                  />
+                </div>
+                <div className="flex-grow p-2">
+                  <p className="text-lg font-semibold">Volmix</p>
+                  <p className="text-xs leading-loose">
+                    A QoL application for Windows sound management system.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <div className="z-3 font-lexend-zetta flex flex-col h-full">
+                <div className="border-primary border-2 p-2 h-57">
+                  <img
+                    className="opacity-60 object-cover w-full h-full"
+                    src={proj2}
+                    alt="image"
+                  />
+                </div>
+                <div className="flex-grow p-2">
+                  <p className="text-lg font-semibold">Chromafit</p>
+                  <p className="text-xs leading-loose">
+                    A clothing brand website.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div
           id="contacts"
           className="h-screen font-prata p-10 flex flex-col justify-center contact-trigger"
         >
-          <div className="sm:block hidden">CONTACTS</div>
           <div className="flex flex-col items-center justify-center flex-grow -mt-20 ">
             <p className="text-center xl:text-3xl text-2xl mb- contact-slide">
               connect with me
             </p>
-            <table className="table-auto mx-auto table-slide">
+            <p>
+              <a
+                className="hover:underline text-2xl underline"
+                href="mailto:agaloos.aaronbruce@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                agaloos.aaronbruce@gmail.com
+              </a>
+            </p>
+            {/* <table className="table-auto mx-auto table-slide">
               <tbody>
                 <tr>
                   <td className="pr-4 text-right">email:</td>
@@ -391,7 +413,7 @@ const App = () => {
                   </td>
                 </tr>
               </tbody>
-            </table>
+            </table> */}
           </div>
         </div>
       </div>
